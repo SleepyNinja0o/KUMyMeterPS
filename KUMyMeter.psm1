b6fc95decf1dd0dffc65ae0e6a8830488f5d9840
SleepyNinja <vb6email@gmail.com>
2023-11-19 14:39:52 -0500

    Improve some variable names...Begin developing New-KUMyMeterWebRequest

--- KUMyMeter.psm1
+++ KUMyMeter.psm1
@@ -12,10 +12,11 @@ $MeterData = Get-KUMyMeterUsageAdvanced -DisplayMeter "All Usage" -UsageRange = 
 
 function Connect-KUMyMeter{
 param($Username,$Password)
-
+    $Global:MyLGEKU_Server = "https://lge-ku.com"
+    $Global:KUMyMeter_Server = "https://mymeter.lge-ku.com"
     #Verify site is reachable and retrieve Form ID + Form Token required for login request
     try{
-        $wr = Invoke-RestMethod "https://lge-ku.com" -UseBasicParsing -SessionVariable Global:KU_Session
+        $wr = Invoke-RestMethod $MyLGEKU_Server -UseBasicParsing -SessionVariable Global:KUMyMeter_Session
         $FormBuildID = ($wr | Select-String -Pattern "name=""form_build_id"" value=""(.*)"" ").Matches.Groups[1].value
         $FormToken = ($wr | Select-String -Pattern "name=""form_token"" value=""(.*)"" ").Matches.Groups[1].value
     }catch{
@@ -49,7 +50,7 @@ param($Username,$Password)
             'remote' = ''
         }
         try{
-            $wr1 = Invoke-WebRequest -UseBasicParsing -Uri "https://my.lge-ku.com/cs/logon.sap" -Method "POST" -WebSession $KU_Session -ContentType "application/x-www-form-urlencoded" -Body $KUMyMeter_Login_Data
+            $wr1 = Invoke-WebRequest -UseBasicParsing -Uri "$MyLGEKU_Server/cs/logon.sap" -Method "POST" -WebSession $KUMyMeter_Session -ContentType "application/x-www-form-urlencoded" -Body $KUMyMeter_Login_Data
             Remove-Variable -Name Password -Force -ErrorAction SilentlyContinue
             Remove-Variable -Name KUMyMeter_Login_Data -Force -ErrorAction SilentlyContinue
         }catch{
@@ -69,19 +70,19 @@ param($Username,$Password)
     }
 
     #Start KU account selection process
-    $wr2 = Invoke-RestMethod "https://my.lge-ku.com/cs/doSwitch.sap" -WebSession $KU_Session
+    $wr2 = Invoke-RestMethod "$MyLGEKU_Server/cs/doSwitch.sap" -WebSession $KUMyMeter_Session
     $wr2 = $wr2.Substring($wr2.IndexOf("frmSelectAccount"))
     $xsrfid = [System.Net.WebUtility]::UrlEncode($(($wr2 | Select-String -Pattern "name=""xsrfid"" value=""(.*)""/").Matches.Groups[1].value))
     $as_sfid = [System.Net.WebUtility]::UrlEncode($(($wr2 | Select-String -Pattern "name=""as_sfid"" value=""(.*)"" /><input").Matches.Groups[1].value))
     $as_fid = [System.Net.WebUtility]::UrlEncode($(($wr2 | Select-String -Pattern "name=""as_fid"" value=""(.*)"" /").Matches.Groups[1].value))
 
     #Pull all sub-accounts(places) associated with current KU account
-    $KUAccounts = Invoke-RestMethod "https://my.lge-ku.com/cs/getAcctListAjax.ajax" -WebSession $KU_Session -Method Post -ContentType "application/x-www-form-urlencoded; charset=UTF-8" -Headers @{
+    $KUAccounts = Invoke-RestMethod "$MyLGEKU_Server/cs/getAcctListAjax.ajax" -WebSession $KUMyMeter_Session -Method Post -ContentType "application/x-www-form-urlencoded; charset=UTF-8" -Headers @{
       "accept"="application/json, text/javascript, */*; q=0.01"
       "accept-encoding"="gzip, deflate, br"
       "accept-language"="en-US,en;q=0.9"
-      "Origin"="https://my.lge-ku.com"
-      "Referer"="https://my.lge-ku.com/cs/doSwitch.sap"
+      "Origin"=$MyLGEKU_Server
+      "Referer"="$MyLGEKU_Server/cs/doSwitch.sap"
       "x-requested-with"="XMLHttpRequest"
     } -Body "xsrfid=$xsrfid"
 
@@ -102,40 +103,68 @@ param($Username,$Password)
     }
 
     #Switch to the KU account chosen above
-    $wr3 = Invoke-WebRequest -UseBasicParsing -Uri "https://my.lge-ku.com/cs/accswitch.sap" -Method "POST" -WebSession $KU_Session -Headers @{
+    $wr3 = Invoke-WebRequest -UseBasicParsing -Uri "$MyLGEKU_Server/cs/accswitch.sap" -Method "POST" -WebSession $KUMyMeter_Session -Headers @{
     "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
       "Accept-Encoding"="gzip, deflate, br"
       "Accept-Language"="en-US,en;q=0.9"
-      "Origin"="https://my.lge-ku.com"
-      "Referer"="https://my.lge-ku.com/cs/doSwitch.sap"
+      "Origin"=$MyLGEKU_Server
+      "Referer"="$MyLGEKU_Server/cs/doSwitch.sap"
     } -ContentType "application/x-www-form-urlencoded" -Body "xsrfid=$xsrfid&accselect=$AccNum&as_sfid=$as_sfid&as_fid=$as_fid"
     
     #Pull KU
-    $wr4 = Invoke-WebRequest "https://my.lge-ku.com/cs/doStart.sap" -WebSession $KU_Session
+    $wr4 = Invoke-WebRequest "$MyLGEKU_Server/cs/doStart.sap" -WebSession $KUMyMeter_Session
 
     #Gather required parameters from the KU website to authenticate to the MyMeter application
-    $wr5 = Invoke-RestMethod "https://my.lge-ku.com/cs/b.ee_ams_mymeter.sap" -WebSession $KU_Session
+    $wr5 = Invoke-RestMethod "$MyLGEKU_Server/cs/b.ee_ams_mymeter.sap" -WebSession $KUMyMeter_Session
     $MyMeterAuthURL = ($wr5 | Select-String -Pattern "action=""(.*)"" ").Matches.Groups[1].value
-    $xsrfid2 = [System.Net.WebUtility]::UrlEncode($(($wr5 | Select-String -Pattern "name=""xsrfid"" value=""(.*)""/").Matches.Groups[1].value))
-    $as_sfid2 = [System.Net.WebUtility]::UrlEncode($(($wr5 | Select-String -Pattern "name=""as_sfid"" value=""(.*)"" /><input").Matches.Groups[1].value))
-    $as_fid2 = [System.Net.WebUtility]::UrlEncode($(($wr5 | Select-String -Pattern "name=""as_fid"" value=""(.*)"" /").Matches.Groups[1].value))
+    #$xsrfid2 = [System.Net.WebUtility]::UrlEncode($(($wr5 | Select-String -Pattern "name=""xsrfid"" value=""(.*)""/").Matches.Groups[1].value))
+    #$as_sfid2 = [System.Net.WebUtility]::UrlEncode($(($wr5 | Select-String -Pattern "name=""as_sfid"" value=""(.*)"" /><input").Matches.Groups[1].value))
+    #$as_fid2 = [System.Net.WebUtility]::UrlEncode($(($wr5 | Select-String -Pattern "name=""as_fid"" value=""(.*)"" /").Matches.Groups[1].value))
+    #$JSONRequest = $wr5.Substring($wr5.IndexOf("JSON.stringify")+15)
+    #$JSONRequest = $JSONRequest.Substring(0,$JSONRequest.IndexOf("})")+1)
+    #$JSONRequest = [System.Net.WebUtility]::UrlEncode($($JSONRequest | ConvertFrom-Json | ConvertTo-Json -Compress))
+
+    $xsrfid = ($wr5 | Select-String -Pattern "name=""xsrfid"" value=""(.*)""/").Matches.Groups[1].value
+    $as_sfid = ($wr5 | Select-String -Pattern "name=""as_sfid"" value=""(.*)"" /><input").Matches.Groups[1].value
+    $as_fid = ($wr5 | Select-String -Pattern "name=""as_fid"" value=""(.*)"" /").Matches.Groups[1].value
     $JSONRequest = $wr5.Substring($wr5.IndexOf("JSON.stringify")+15)
     $JSONRequest = $JSONRequest.Substring(0,$JSONRequest.IndexOf("})")+1)
-    $JSONRequest = [System.Net.WebUtility]::UrlEncode($($JSONRequest | ConvertFrom-Json | ConvertTo-Json -Compress))
+    $JSONRequest = $JSONRequest | ConvertFrom-Json
+    $wr6Request = [Ordered]@{
+        'xsrfid' = $xsrfid
+        'request' = ($JSONRequest | ConvertTo-Json -Compress)
+        'as_sfid' = $as_sfid
+        'as_fid2' = $as_fid
+    }
+
+    #"xsrfid=$xsrfid2&request=$JSONRequest&as_sfid=$as_sfid2&as_fid=$as_fid2"
 
     #Authenticate to MyMeter via the external authentication URL pulled from KU above
-    $wr6 = Invoke-WebRequest -UseBasicParsing -Uri $MyMeterAuthURL -WebSession $KU_Session -Method Post -ContentType "application/x-www-form-urlencoded" -Body "xsrfid=$xsrfid2&request=$JSONRequest&as_sfid=$as_sfid2&as_fid=$as_fid2"
-    $wr7 = Invoke-WebRequest -UseBasicParsing -Uri "https://mymeter.lge-ku.com/" -WebSession $KU_Session
-    $wr8 = Invoke-WebRequest -UseBasicParsing -Uri "https://mymeter.lge-ku.com/Dashboard" -WebSession $KU_Session
+    #$wr6 = Invoke-WebRequest -UseBasicParsing -Uri $MyMeterAuthURL -WebSession $KUMyMeter_Session -Method Post -ContentType "application/x-www-form-urlencoded" -Body "xsrfid=$xsrfid2&request=$JSONRequest&as_sfid=$as_sfid2&as_fid=$as_fid2"
+    $wr6 = Invoke-WebRequest -UseBasicParsing -Uri $MyMeterAuthURL -WebSession $KUMyMeter_Session -Method Post -ContentType "application/x-www-form-urlencoded" -Body $wr6Request
+    $wr7 = Invoke-WebRequest -UseBasicParsing -Uri $KUMyMeter_Server -WebSession $KUMyMeter_Session
+    $wr8 = Invoke-WebRequest -UseBasicParsing -Uri "$KUMyMeter_Server/Dashboard" -WebSession $KUMyMeter_Session
+
+    $MyMeterPage = New-Object -ComObject "HTMLFile"
+    $MyMeterPage.IHTMLDocument2_write($wr8.Content)
+    $MyMeterAccountDetails = ($MyMeterPage.getElementsByTagName("div") | where {$_.className -match "col " -and $_.innerText -match "\n"}).innerText.Trim()
+
+    Write-Host "MyMeter login success!" -ForegroundColor Green
+    Write-Host "`nMyMeter account details:" -ForegroundColor Cyan
+    Write-Host "$MyMeterAccountDetails`n"
 
     $Script:RequestVerificationToken = ($wr8.Content | Select-String -Pattern "name=""__RequestVerificationToken"".*value=""(.*)"" /").Matches.Groups[1].Value
+}
+
+function Get-KUMyMeterRegisteredUsers{
+    $KUMyMeter_UserInformation = Invoke-RestMethod "$KUMyMeter_Server/User/Information"
 }
 
 function Get-KUMyMeterMeters{
     #Generate current timestamp (Unix epoch) and request usage statistics from MyMeter
     $timestamp = (get-date -UFormat "%s") -replace "\."
     $timestamp = $timestamp.Substring(0,$timestamp.Length-2)
-    $wr9 = Invoke-WebRequest -UseBasicParsing -Uri "https://mymeter.lge-ku.com/Dashboard/Table?_=$timestamp" -WebSession $KU_Session -Headers @{
+    $wr9 = Invoke-WebRequest -UseBasicParsing -Uri "$KUMyMeter_Server/Dashboard/Table?_=$timestamp" -WebSession $KUMyMeter_Session -Headers @{
     "authority"="mymeter.lge-ku.com"
       "method"="GET"
       "path"="/Dashboard/Table?_=$timestamp"
@@ -174,7 +203,7 @@ function Get-KUMyMeterUsage{
     #Generate current timestamp (Unix epoch) and request usage statistics from MyMeter table
     $timestamp = (get-date -UFormat "%s") -replace "\."
     $timestamp = $timestamp.Substring(0,$timestamp.Length-2)
-    $wr9 = Invoke-WebRequest -UseBasicParsing -Uri "https://mymeter.lge-ku.com/Dashboard/Table?_=$timestamp" -WebSession $KU_Session -Headers @{
+    $wr9 = Invoke-WebRequest -UseBasicParsing -Uri "$KUMyMeter_Server/Dashboard/Table?_=$timestamp" -WebSession $KUMyMeter_Session -Headers @{
     "authority"="mymeter.lge-ku.com"
       "method"="GET"
       "path"="/Dashboard/Table?_=$timestamp"
@@ -267,9 +296,9 @@ $UsageType = "Dollar($)"
         }
     }
 
-    $wr10 = Invoke-WebRequest -UseBasicParsing -Uri "https://mymeter.lge-ku.com/Dashboard/Table" `
+    $wr10 = Invoke-WebRequest -UseBasicParsing -Uri "$KUMyMeter_Server/Dashboard/Table" `
     -Method "POST" `
-    -WebSession $KU_Session `
+    -WebSession $KUMyMeter_Session `
     -Headers @{
     "authority"="mymeter.lge-ku.com"
       "method"="POST"
@@ -278,8 +307,8 @@ $UsageType = "Dollar($)"
       "accept"="text/plain, */*; q=0.01"
       "accept-encoding"="gzip, deflate, br"
       "accept-language"="en-US,en;q=0.9"
-      "origin"="https://mymeter.lge-ku.com"
-      "referer"="https://mymeter.lge-ku.com/Dashboard"
+      "origin"=$KUMyMeter_Server
+      "referer"="$KUMyMeter_Server/Dashboard"
       "x-requested-with"="XMLHttpRequest"
     } `
     -ContentType "application/x-www-form-urlencoded; charset=UTF-8" `
@@ -296,4 +325,47 @@ $UsageType = "Dollar($)"
     }
 
     return $MeterData
+}
+
+function New-KUMyMeterWebRequest{
+param(
+    $Endpoint,
+    [ValidateSet("Get","Post")]$Method="Get",
+    $ContentType="",
+    $Body,
+    [switch]$REST
+)
+    if (!$KUMyMeter_Session){Connect-KUMyMeter}
+
+    for($Retry=1;$Retry -le 3;$Retry++){
+        try{
+            if($REST.IsPresent){
+                if($Body -ne $null -and $Body -ne ""){
+                    $ServiceNow_WR = Invoke-RestMethod -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
+                    -Method $Method -ContentType $ContentType -Body $Body
+                }else{
+                    $ServiceNow_WR = Invoke-RestMethod -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
+                    -Method $Method
+                }
+            }else{
+                if($Body -ne $null -and $Body -ne ""){
+                    $ServiceNow_WR = Invoke-WebRequest -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
+                    -Method $Method -ContentType $ContentType -Body $Body
+                }else{
+                    $ServiceNow_WR = Invoke-WebRequest -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
+                    -Method $Method
+                }
+            }
+            return $ServiceNow_WR
+        }catch{
+            if($Retry -eq 3){
+                Write-Host "Failed to submit web request 3 times in a row...Try again?(y/n): " -ForegroundColor Red -NoNewline
+                $resp = Read-Host
+                if($resp.ToLower() -match "y|yes"){$Retry=0}else{return}
+            }else{
+                Write-Host "Error occured while submitting web request to SNOW! Retrying..." -ForegroundColor Yellow
+                Start-Sleep -Seconds 2
+            }
+        }
+    }
 }
