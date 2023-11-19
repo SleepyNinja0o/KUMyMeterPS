39a34298bb2c577445b06e8ae78b373d8630c6a3
SleepyNinja <vb6email@gmail.com>
2023-11-19 17:43:10 -0500

    Add 3 new funcs Get-KUMyMeterAccessLog,Get-KUMyMeterRegisteredUsers,Get-KUMyMeterAdditionalUsers

--- KUMyMeter.psm1
+++ KUMyMeter.psm1
@@ -10,15 +10,29 @@ $MeterData = Get-KUMyMeterUsage
 $MeterData = Get-KUMyMeterUsageAdvanced -DisplayMeter "All Usage" -UsageRange = "FifteenMinByDay" -UsageType = "Dollar($)"
 #>
 
+function Parse-String ($String,$StartStr,$EndStr){
+    if($String.IndexOf($StartStr) -eq -1){return ""}
+    $StartStrPos = $String.IndexOf($StartStr)+$StartStr.Length
+
+    if($String.IndexOf($EndStr,$StartStrPos) -eq -1){return ""}
+    $EndStrPos = $String.IndexOf($EndStr,$StartStrPos)
+
+    $ParsedStr = $String.Substring($StartStrPos,$EndStrPos-$StartStrPos)
+    if($ParsedStr -ne "" -and $ParsedStr -ne $null){return $ParsedStr}
+
+    return ""
+}
+
 function Connect-KUMyMeter{
 param($Username,$Password)
-    $Global:MyLGEKU_Server = "https://lge-ku.com"
+    $Global:LGEKU_Server = "https://lge-ku.com"
+    $Global:MyLGEKU_Server = "https://my.lge-ku.com"
     $Global:KUMyMeter_Server = "https://mymeter.lge-ku.com"
     #Verify site is reachable and retrieve Form ID + Form Token required for login request
     try{
-        $wr = Invoke-RestMethod $MyLGEKU_Server -UseBasicParsing -SessionVariable Global:KUMyMeter_Session
-        $FormBuildID = ($wr | Select-String -Pattern "name=""form_build_id"" value=""(.*)"" ").Matches.Groups[1].value
-        $FormToken = ($wr | Select-String -Pattern "name=""form_token"" value=""(.*)"" ").Matches.Groups[1].value
+        $global:wr = Invoke-RestMethod $LGEKU_Server -UseBasicParsing -SessionVariable Global:KUMyMeter_Session
+        $global:FormBuildID = ($wr | Select-String -Pattern "name=""form_build_id"" value=""(.*)"" ").Matches.Groups[1].value
+        $global:FormToken = ($wr | Select-String -Pattern "name=""form_token"" value=""(.*)"" ").Matches.Groups[1].value
     }catch{
         Write-Host "Unable to reach the LG&E KU login page!`n$($_.Exception.Message)" -ForegroundColor Red
         return
@@ -50,7 +64,7 @@ param($Username,$Password)
             'remote' = ''
         }
         try{
-            $wr1 = Invoke-WebRequest -UseBasicParsing -Uri "$MyLGEKU_Server/cs/logon.sap" -Method "POST" -WebSession $KUMyMeter_Session -ContentType "application/x-www-form-urlencoded" -Body $KUMyMeter_Login_Data
+            $global:wr1 = Invoke-WebRequest -UseBasicParsing -Uri "$MyLGEKU_Server/cs/logon.sap" -Method "POST" -WebSession $KUMyMeter_Session -ContentType "application/x-www-form-urlencoded" -Body $KUMyMeter_Login_Data
             Remove-Variable -Name Password -Force -ErrorAction SilentlyContinue
             Remove-Variable -Name KUMyMeter_Login_Data -Force -ErrorAction SilentlyContinue
         }catch{
@@ -156,8 +170,92 @@ param($Username,$Password)
     $Script:RequestVerificationToken = ($wr8.Content | Select-String -Pattern "name=""__RequestVerificationToken"".*value=""(.*)"" /").Matches.Groups[1].Value
 }
 
+function Get-KUMyMeterAccessLog{
+param([switch]$Get50Rows)
+    if(!$KUMyMeter_UserInformation){
+        $Global:KUMyMeter_UserInformation = (New-KUMyMeterWebRequest -Endpoint "/User/Information").Content
+    }
+
+    $KUMyMeter_HTMLOb = New-Object -ComObject "HTMLFile"
+    $KUMyMeter_HTMLOb.IHTMLDocument2_write($KUMyMeter_UserInformation)
+    $KUMyMeter_RequestVerificationToken = ($KUMyMeter_HTMLOb.getElementsByTagName("input") | where {$_.name -eq "__RequestVerificationToken"})[0].value
+
+    if($Get50MoreRows.IsPresent){
+       $KUMyMeter_UserInformation = (New-KUMyMeterWebRequest -Endpoint "/User/LoadMoreActivities" -Method Post -Headers @{"x-requested-with"="XMLHttpRequest"} -ContentType "application/x-www-form-urlencoded; charset=UTF-8" -Body "rowCount=50&__RequestVerificationToken=$KUMyMeter_RequestVerificationToken" -REST).AjaxResults.Value
+       $KUMyMeter_HTMLOb = New-Object -ComObject "HTMLFile"
+       $KUMyMeter_HTMLOb.IHTMLDocument2_write($KUMyMeter_UserInformation)
+       $KUMyMeter_RequestVerificationToken = ($KUMyMeter_HTMLOb.getElementsByTagName("input") | where {$_.name -eq "__RequestVerificationToken"})[0].value
+    }
+
+    $KUMyMeter_AccessLogTable = ($KUMyMeter_HTMLOb.getElementsByTagName("h4") | where {$_.innerText -match "Access Log"}).parentElement.getElementsByTagName("table")[0]
+
+    $KUMyMeter_AccessLogHeaders = $KUMyMeter_AccessLogTable.getElementsByTagName("th") | foreach {$_.innerText}
+
+    $KUMyMeter_AccessLogRows = $KUMyMeter_AccessLogTable.getElementsByTagName("tr")
+
+    $KUMyMeter_AccessLogDataObject = [pscustomobject]@{}
+    $KUMyMeter_AccessLogHeaders | foreach {$KUMyMeter_AccessLogDataObject | Add-Member -MemberType NoteProperty -Name $_ -Value ""}
+
+    $KUMyMeter_AccessLogData = @()
+    $KUMyMeter_AccessLogRows | foreach {$rows=$_.getElementsByTagName("td");$i=0;$KUMyMeter_AccessLogEntry=$KUMyMeter_AccessLogDataObject.psobject.Copy();$rows | foreach {$KUMyMeter_AccessLogEntry.$($KUMyMeter_AccessLogHeaders[$i]) = $($_.innerText);$i++};if($rows.length -ne 0 -and $KUMyMeter_AccessLogEntry.User -notmatch "View More"){$KUMyMeter_AccessLogData += $KUMyMeter_AccessLogEntry}}
+
+    return $KUMyMeter_AccessLogData
+}
+
+function Get-KUMyMeterAdditionalUsers{
+    if(!$KUMyMeter_UserInformation){
+        $Global:KUMyMeter_UserInformation = (New-KUMyMeterWebRequest -Endpoint "/User/Information").Content
+    }
+
+    $KUMyMeter_HTMLOb = New-Object -ComObject "HTMLFile"
+    $KUMyMeter_HTMLOb.IHTMLDocument2_write($KUMyMeter_UserInformation)
+    $KUMyMeter_RequestVerificationToken = ($KUMyMeter_HTMLOb.getElementsByTagName("input") | where {$_.name -eq "__RequestVerificationToken"})[0].value
+
+    #$KUMyMeter_TableSections = $KUMyMeter_HTMLOb.getElementsByTagName("h4") | where {$_.innerText -match "Registered Users|Additional User Access|Access Log"} | foreach {$_.parentElement}
+
+    try{
+        $KUMyMeter_AdditionalUsersTable = ($KUMyMeter_HTMLOb.getElementsByTagName("h4") | where {$_.innerText -match "Additional User Access"}).parentElement.getElementsByTagName("table")[0]
+    }catch{
+        return "No additional user access exists!"
+    }
+
+    $KUMyMeter_AdditionalUsersHeaders = $KUMyMeter_AdditionalUsersTable.getElementsByTagName("th") | foreach {$_.innerText}
+
+    $KUMyMeter_AdditionalUsersRows = $KUMyMeter_AdditionalUsersTable.getElementsByTagName("tr")
+
+    $KUMyMeter_AdditionalUserDataObject = [pscustomobject]@{}
+    $KUMyMeter_AdditionalUsersHeaders | foreach {$KUMyMeter_AdditionalUserDataObject | Add-Member -MemberType NoteProperty -Name $_ -Value ""}
+
+    $KUMyMeter_AdditionalUsersData = @()
+    $KUMyMeter_AdditionalUsersRows | foreach {$rows=$_.getElementsByTagName("td");$i=0;$KUMyMeter_AdditionalUser=$KUMyMeter_AdditionalUserDataObject.psobject.Copy();$rows | foreach {$KUMyMeter_AdditionalUser.$($KUMyMeter_AdditionalUsersHeaders[$i]) = $($_.innerText);$i++};if($rows.length -ne 0){$KUMyMeter_AdditionalUsersData += $KUMyMeter_AdditionalUser}}
+
+    return $KUMyMeter_AdditionalUsersData
+}
+
 function Get-KUMyMeterRegisteredUsers{
-    $KUMyMeter_UserInformation = Invoke-RestMethod "$KUMyMeter_Server/User/Information"
+    if(!$KUMyMeter_UserInformation){
+        $Global:KUMyMeter_UserInformation = (New-KUMyMeterWebRequest -Endpoint "/User/Information").Content
+    }
+
+    $KUMyMeter_HTMLOb = New-Object -ComObject "HTMLFile"
+    $KUMyMeter_HTMLOb.IHTMLDocument2_write($KUMyMeter_UserInformation)
+    $KUMyMeter_RequestVerificationToken = ($KUMyMeter_HTMLOb.getElementsByTagName("input") | where {$_.name -eq "__RequestVerificationToken"})[0].value
+
+    #$KUMyMeter_TableSections = $KUMyMeter_HTMLOb.getElementsByTagName("h4") | where {$_.innerText -match "Registered Users|Additional User Access|Access Log"} | foreach {$_.parentElement}
+
+    $KUMyMeter_RegisteredUsersTable = ($KUMyMeter_HTMLOb.getElementsByTagName("h4") | where {$_.innerText -match "Registered Users"}).parentElement.getElementsByTagName("table")[0]
+
+    $KUMyMeter_RegisteredUsersHeaders = $KUMyMeter_RegisteredUsersTable.getElementsByTagName("th") | foreach {$_.innerText}
+
+    $KUMyMeter_RegisteredUsersRows = $KUMyMeter_RegisteredUsersTable.getElementsByTagName("tr")
+
+    $KUMyMeter_RegisteredUserDataObject = [pscustomobject]@{}
+    $KUMyMeter_RegisteredUsersHeaders | foreach {$KUMyMeter_RegisteredUserDataObject | Add-Member -MemberType NoteProperty -Name $_ -Value ""}
+
+    $KUMyMeter_RegisteredUsersData = @()
+    $KUMyMeter_RegisteredUsersRows | foreach {$rows=$_.getElementsByTagName("td");$i=0;$KUMyMeter_RegisteredUser=$KUMyMeter_RegisteredUserDataObject.psobject.Copy();$rows | foreach {$KUMyMeter_RegisteredUser.$($KUMyMeter_RegisteredUsersHeaders[$i]) = $($_.innerText);$i++};if($rows.length -ne 0){$KUMyMeter_RegisteredUsersData += $KUMyMeter_RegisteredUser}}
+
+    return $KUMyMeter_RegisteredUsersData
 }
 
 function Get-KUMyMeterMeters{
@@ -329,10 +427,11 @@ $UsageType = "Dollar($)"
 
 function New-KUMyMeterWebRequest{
 param(
-    $Endpoint,
+    $Endpoint="",
     [ValidateSet("Get","Post")]$Method="Get",
-    $ContentType="",
-    $Body,
+    $Headers=@{},
+    $ContentType,
+    $Body="",
     [switch]$REST
 )
     if (!$KUMyMeter_Session){Connect-KUMyMeter}
@@ -341,31 +440,33 @@ param(
         try{
             if($REST.IsPresent){
                 if($Body -ne $null -and $Body -ne ""){
-                    $ServiceNow_WR = Invoke-RestMethod -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
-                    -Method $Method -ContentType $ContentType -Body $Body
+                    $KUMyMeter_WR = Invoke-RestMethod -UseBasicParsing "$KUMyMeter_Server$Endpoint" -WebSession $KUMyMeter_Session `
+                    -Headers $Headers -Method $Method -ContentType $ContentType -Body $Body
                 }else{
-                    $ServiceNow_WR = Invoke-RestMethod -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
-                    -Method $Method
+                    $KUMyMeter_WR = Invoke-RestMethod -UseBasicParsing "$KUMyMeter_Server$Endpoint" -WebSession $KUMyMeter_Session `
+                    -Headers $Headers -Method $Method
                 }
             }else{
                 if($Body -ne $null -and $Body -ne ""){
-                    $ServiceNow_WR = Invoke-WebRequest -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
-                    -Method $Method -ContentType $ContentType -Body $Body
+                    $KUMyMeter_WR = Invoke-WebRequest -UseBasicParsing "$KUMyMeter_Server$Endpoint" -WebSession $KUMyMeter_Session `
+                    -Headers $Headers -Method $Method -ContentType $ContentType -Body $Body
                 }else{
-                    $ServiceNow_WR = Invoke-WebRequest -UseBasicParsing "https://$ServiceNow_Server$Endpoint" -WebSession $ServiceNow_Session `
-                    -Method $Method
+                    $KUMyMeter_WR = Invoke-WebRequest -UseBasicParsing "$KUMyMeter_Server$Endpoint" -WebSession $KUMyMeter_Session `
+                    -Headers $Headers -Method $Method
                 }
             }
-            return $ServiceNow_WR
+            return $KUMyMeter_WR
         }catch{
             if($Retry -eq 3){
                 Write-Host "Failed to submit web request 3 times in a row...Try again?(y/n): " -ForegroundColor Red -NoNewline
                 $resp = Read-Host
                 if($resp.ToLower() -match "y|yes"){$Retry=0}else{return}
             }else{
-                Write-Host "Error occured while submitting web request to SNOW! Retrying..." -ForegroundColor Yellow
+                Write-Host "Error occured while submitting web request to KU MyMeter! Retrying..." -ForegroundColor Yellow
                 Start-Sleep -Seconds 2
             }
         }
     }
 }
+
+Export-ModuleMember -Function Connect-KUMyMeter,Get-KUMyMeterAccessLog,Get-KUMyMeterAdditionalUsers,Get-KUMyMeterRegisteredUsers,Get-KUMyMeterMeters,Get-KUMyMeterUsage,Get-KUMyMeterUsageAdvanced,New-KUMyMeterWebRequest
